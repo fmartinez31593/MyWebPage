18b8f63cd0b4748bf0d05007972dbad0289df650
fmartinezr <111297630+fmartinez31593@users.noreply.github.com>
2023-02-20 15:16:21 -0600

    first commit

---
+++ application/views/welcome/welcome_script.php
@@ -0,0 +1,7 @@
+<script type="text/javascript">
+$(document).ready(function(){
+    setTimeout(() => {
+        swal("Good job!", "You clicked the button!", "success");
+    }, 1000);
+});
+</script>
---
+++ application/views/welcome/welcome_view.php
@@ -0,0 +1,13 @@
+<main>
+    <div class="row">
+        <div class="col s12">
+            <div class="card hoverable">
+                <div class="card-content">
+                    <span class="card-title center-align">Welcome</span>
+                    <p><?= print_r($users) ?></p>
+                </div>
+                <div class="card-action"></div>
+            </div>
+        </div>    
+    </div>
+</main>
